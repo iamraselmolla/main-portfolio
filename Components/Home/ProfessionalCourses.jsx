aee95f551e32f8669707bc73aa4170bcb66aa9d3
iamraselmolla <raselmolla6336@gmail.com>
2024-09-04 20:53:05 +0600

    edited the course

--- Components/Home/ProfessionalCourses.jsx
+++ Components/Home/ProfessionalCourses.jsx
@@ -2,6 +2,22 @@ import React from "react";
 import { FaCertificate } from "react-icons/fa";
 
 export default function ProfessionalCourses() {
+    const courses = [{
+        date: "June 2022 - Dec 2022",
+        title: "Programming Hero",
+        details: "Complete Web Development Course With Jhankar Mahbub",
+        skills: ["HTML5", "CSS3", "JavaScript", "React", "Node.js", "Express", "MongoDB", "Git", "Github", "Vercel", "Firebase"]
+    }, {
+        date: "Sep 2020 - Jun 2021",
+        title: "Udemy.com",
+        details: "The Complete JavaScript Course: From Zero to Expert! by Jonas Schmedtmann",
+        skills: ["JavaScript", "Asynchronous JavaScript", "JavaScript Fundamentals", "Advanced JavaScript"]
+    }, {
+        date: "Nov 2016 - Apr 2017",
+        title: "Computer Palace, Khalispur, Khulna",
+        details: "Web Design and Development",
+        skills: ["HTML", "CSS", "Basic JavaScript", "Web Design Principles", "Basic PHP", "WordPress Customization", "WordPress Development"]
+    }]
     return (
         <div className="bg-gray-100 dark:bg-gray-800 py-12 px-4 md:px-10 w-full">
             <div className="flex items-center pb-8">
@@ -13,22 +29,7 @@ export default function ProfessionalCourses() {
             <div className="relative">
                 <div className="absolute left-1/2 top-0 w-1 bg-blue-300 dark:bg-blue-600 transform -translate-x-1/2 h-full"></div>
                 <div className="space-y-8">
-                    {[{
-                        date: "June 2022 - Dec 2022",
-                        title: "Programming Hero",
-                        details: "Complete Web Development Course With Jhankar Mahbub",
-                        skills: ["HTML5", "CSS3", "JavaScript", "React", "Node.js", "Express", "MongoDB", "Git", "Github", "Vercel", "Firebase"]
-                    }, {
-                        date: "Sep 2020 - Jun 2021",
-                        title: "Udemy.com",
-                        details: "The Complete JavaScript Course: From Zero to Expert! by Jonas Schmedtmann",
-                        skills: ["JavaScript", "Asynchronous JavaScript", "JavaScript Fundamentals", "Advanced JavaScript"]
-                    }, {
-                        date: "Nov 2016 - Apr 2017",
-                        title: "Computer Palace, Khalispur, Khulna",
-                        details: "Web Design and Development",
-                        skills: ["HTML", "CSS", "Basic JavaScript", "Web Design Principles", "Basic PHP", "WordPress Customization", "WordPress Development"]
-                    }].map((course, index) => (
+                    {courses.map((course, index) => (
                         <div key={index} className="relative flex items-start bg-white dark:bg-gray-900 p-6 shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300">
                             <div className="absolute left-[-20px] top-1/2 transform -translate-y-1/2 w-12 h-12 bg-blue-600 dark:bg-blue-400 rounded-full flex items-center justify-center text-white text-2xl">
                                 <FaCertificate />
